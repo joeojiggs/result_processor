b696eb661b923140b9d646381abbeea2f8f0f574
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-08 15:16:33 +0100

    Gitacora saved 1 changed files

--- result.php
+++ result.php
@@ -85,7 +85,7 @@ $gender = ($_POST['gender']);
                 
                     <div class="col-lg-6">
                         <p>
-                            Students' Name: <u><?php echo $stud_name?></u>
+                            Students' Name: <u>&nbsp<?php echo $stud_name?></u>
                         </p>
                     </div>
                         
