5b09f16e042614023f123edeeab0c0302f0e19f3
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-08 15:17:49 +0100

    Gitacora saved 1 changed files

--- result.php
+++ result.php
@@ -109,7 +109,7 @@ $gender = ($_POST['gender']);
                     
                     <div class="col-lg-6">
                         <p>
-                            REG Number: 
+                            REG Number: <u>&nbsp<?php echo $class_name?>&nbsp</u>
                         </p>
                     </div>
                     
