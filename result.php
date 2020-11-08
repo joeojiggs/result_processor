da3c647e1a0cdaecc7dc7222f48ba891f3a0cd4c
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-08 02:34:19 +0100

    Gitacora saved 1 changed files

--- result.php
+++ result.php
@@ -71,11 +71,7 @@ $reg_no = ($_POST['reg_no']);
                     <p align = "center">school website if any</p>
 
                     <h3 align = "center">TERMINAL REPORT CARD</h3>
-                    <hr style </p>
-                    </div>
-                        
-                    <div class="col-lg-6">
-                        <p>>
+                    <hr style >
 
                     <div class="col-lg-6">
                         <p>
