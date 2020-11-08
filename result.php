e93ec5b7d1481fe7d9d6256f2623633aaa97865e
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-08 16:00:48 +0100

    Gitacora saved 1 changed files

--- result.php
+++ result.php
@@ -187,7 +187,7 @@ $num_of_sub = mysqli_num_rows($query);
 
                     while ($thisrow1 = mysqli_fetch_array($thisquery1)){
                         $sub_id = $thisrow1['subject_name'];
-                        echo 
+                        echo $
                         ?>
                         <tr>
                         <td><?php echo $i ?></td>
