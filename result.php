9fb349f3b2fe47dbfa2b5f7bff2ddee82edddc3c
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-08 15:58:44 +0100

    Gitacora saved 1 changed files

--- result.php
+++ result.php
@@ -204,7 +204,7 @@ $num_of_sub = mysqli_num_rows($query);
                         ?>
                         </td>
                         <td>
-                        <??>
+                        <?php echo $row['first test']; ?>
                             
                         </td>
                     
