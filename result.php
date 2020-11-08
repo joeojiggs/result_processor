415a989123fc60e77cc553c6d93a4650cd58c309
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-08 03:18:18 +0100

    Gitacora saved 1 changed files

--- result.php
+++ result.php
@@ -244,6 +244,7 @@ $reg_no = ($_POST['reg_no']);
         <div>
             <p>Date:</p>
         </div>
+        
         <!-- Button to Open the Modal -->
         <button type="button" class="btn btn-success me" data-toggle="modal" data-target="#myModal">
         RENDER RESULT
