14d987dd0e60017062e4c9690f7a72d1ba40d457
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-08 15:29:41 +0100

    Gitacora saved 1 changed files

--- result.php
+++ result.php
@@ -18,7 +18,7 @@ $total = $row['total'];
 $grade = $row['grade'];
 
 
-$query = mysqli_query($con, "select * from ".$class_name.)
+$query = mysqli_query($con, "select * from ".$class_name."")
 
 ?>
 
