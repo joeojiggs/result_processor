da83d14e27b7caf7806988565c0b7aca8def0024
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-08 17:58:17 +0100

    Gitacora saved 1 changed files

--- result.php
+++ result.php
@@ -256,7 +256,7 @@ $num_of_sub = mysqli_num_rows($query);
                         <td>
                         <?php echo $grade ;
                             if  ($grade == "A"){
-                             v   
+                                echo "&nbsp &nbsp(Excellent)";   
                             }elseif  ($grade == "B"){
                                 
                             }elseif  ($grade == "C"){
