ac11cfc9c8a0d46c98ac5e37eb91ffdc8a53c866
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-08 03:17:38 +0100

    Gitacora saved 1 changed files

--- result.php
+++ result.php
@@ -236,7 +236,7 @@ $reg_no = ($_POST['reg_no']);
         </div>
 
         <div>
-            <p>Principals Comme:</p>
+            <p>Principals Comment:</p>
         </div>
         <!-- Button to Open the Modal -->
         <button type="button" class="btn btn-success me" data-toggle="modal" data-target="#myModal">
