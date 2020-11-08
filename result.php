fa5434b5c28c6d0d9bc9109783f7f39cb52d4e7d
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-08 03:07:52 +0100

    Gitacora saved 1 changed files

--- result.php
+++ result.php
@@ -82,7 +82,7 @@ $reg_no = ($_POST['reg_no']);
                         </p>
                     </div>
                         
-                    <div weclass="col-lg-3">
+                    <div class="col-lg-3">
                         <p>
                             Position: 
                         </p>
