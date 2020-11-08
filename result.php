acc282898b60720e800342d9200ba25a0a27eb90
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-08 02:30:58 +0100

    Gitacora saved 1 changed files

--- result.php
+++ result.php
@@ -111,7 +111,7 @@ $reg_no = ($_POST['reg_no']);
                         </p>
                     </div>
 
-                    <div >
+                    <div class >
 
                     </div>
                     
