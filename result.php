7bae2300a6a32d5c756bf5d4ce342cc89dc19b5a
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-08 15:51:45 +0100

    Gitacora saved 1 changed files

--- result.php
+++ result.php
@@ -196,7 +196,7 @@ $num_of_sub = mysqli_num_rows($query);
                         ?></td>
                         <td>
                         <?php
-                            $query =  mysqli_query($con, "")
+                            $query =  mysqli_query($con, "select ")
                         ?>
                         </td>
                         <td>
