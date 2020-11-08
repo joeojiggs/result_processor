d594e4a4015dc80381a9af9a344410cc26c46afe
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-08 15:24:50 +0100

    Gitacora saved 1 changed files

--- result.php
+++ result.php
@@ -10,7 +10,7 @@ $num_of_stud_in_class = ($_POST['num_of_stud_in_class']);
 
 $query = mysqli_query($con,"select * from students where Class = '$class_name' and Reg_Num = '$reg_no'")
 
-$row 
+$row = my
 
 
 ?>
