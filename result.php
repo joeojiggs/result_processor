d79fa91c9856a8a08bb42a1f9be2951eefc70d82
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-08 16:05:18 +0100

    Gitacora saved 1 changed files

--- result.php
+++ result.php
@@ -123,7 +123,7 @@ $num_of_sub = mysqli_num_rows($query);
                     
                     <div class="col-lg-6">
                         <p>
-                            Regi Number: <u>&nbsp<?php echo $reg_no?>&nbsp</u>
+                            Registration Number: <u>&nbsp<?php echo $reg_no?>&nbsp</u>
                         </p>
                     </div>
                     
