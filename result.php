088ca148e287d088ce73c8c776ac519db2950396
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-08 15:53:42 +0100

    Gitacora saved 1 changed files

--- result.php
+++ result.php
@@ -197,7 +197,7 @@ $num_of_sub = mysqli_num_rows($query);
                         ?></td>
                         <td>
                         <?php
-                            $query =  mysqli_query($con, "select * from ".$class_name."_".$sub_id." where ")
+                            $query =  mysqli_query($con, "select * from ".$class_name."_".$sub_id." where re")
                         ?>
                         </td>
                         <td>
