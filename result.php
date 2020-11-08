a1139175e3291ac957927721337b002a0c1dca5c
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-08 15:42:18 +0100

    Gitacora saved 1 changed files

--- result.php
+++ result.php
@@ -184,7 +184,7 @@ $num_of_sub = mysqli_num_rows($query);
           $thissql1 = "select * from ".$class_name."_subject_list ";
                 $thisquery1 = mysqli_query($con, $thissql1); 
                 if
-                while ($thisrow1 = mysqli_fetch_array($thisquery1));
+                while ($thisrow1 = mysqli_fetch_array($thisquery1))
                 if (isset($thisrow1['subject_name'])){
                     echo $thisrow1['subject_name'];
                 }
