1a691a706a6d3f10a6961e6f053cdd8d322d55c7
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-08 03:13:14 +0100

    Gitacora saved 1 changed files

--- result.php
+++ result.php
@@ -215,7 +215,7 @@ $reg_no = ($_POST['reg_no']);
             <tr>
             <td colspan = "20" align="center">
           <h2> 
-              No Student in Class
+              No Subject  in Class
           </h2>
         </td>
         </tr>
