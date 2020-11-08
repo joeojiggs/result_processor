83bb1ed0238c90e8155198c0ae89608d27350089
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-08 15:18:41 +0100

    Gitacora saved 1 changed files

--- class_tbl.php
+++ class_tbl.php
@@ -26,6 +26,7 @@
             
         $query= mysqli_query($con,"select * from students where Class='$class'");
         if (mysqli_num_rows($query) > 0) {
+            
 
         while($row = mysqli_fetch_array($query)) {
             ?> 
