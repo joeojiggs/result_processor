1c07a9a2b89486a2f38b68fc675fdc0e2942e899
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-08 15:48:01 +0100

    Gitacora saved 1 changed files

--- result.php
+++ result.php
@@ -197,34 +197,7 @@ $num_of_sub = mysqli_num_rows($query);
                     }
                 }
 
-               <td></td>
-               <td>
-               
-                  
-               </td>
-         
-               
-               
-               
-               <td>
-               <?php echo $row['total']; ?>
-               </td>
-               <td>
-               <?php echo $row['avg']; ?>
-               </td>
-               <td>
-               <?php echo $row['grade']; ?>
-               </td>
-               <td>
-               <?php echo $row['position']; ?>
-               </td>
-               
-               
-               
-               </tr>
-            
-          <?php
-          $i++;      
+                    
           }
           } else {
             ?>
