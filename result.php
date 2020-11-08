5241cefd373e6978444c3117a7192a4fe106b34f
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-08 03:08:06 +0100

    Gitacora saved 1 changed files

--- result.php
+++ result.php
@@ -94,7 +94,7 @@ $reg_no = ($_POST['reg_no']);
                         </p>
                     </div>
                         
-                    <div class="col-lg-6">
+                    <div class="well col-lg-6">
                         <p>
                             N0 of Students: 
                         </p>
