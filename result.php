18dda89ea407499ca22163adbb86204f61460abc
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-08 02:21:57 +0100

    Gitacora saved 1 changed files

--- result.php
+++ result.php
@@ -73,7 +73,7 @@ $reg_no = ($_POST['reg_no']);
                     <h3 align = "center">TERMINAL REPORT CARD</h3>
                     <hr>
 
-                    <table tabl>
+                    <table table>
                     <tr>
                     <td>
                         <p>
