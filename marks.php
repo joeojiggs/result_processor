9170e38a68da3b0b00de1a731ee3e3f73cef343a
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-08 17:20:18 +0100

    Gitacora saved 1 changed files

--- marks.php
+++ marks.php
@@ -55,6 +55,7 @@ if (isset($_POST['grade'])){
                         $position = $i."ND" ;
 
                         $sql3 = "update ".$class_name."_".$sub_id." set `grade` = '$grade', `position` = '$position' where reg_no = '$reg_no'";
+                        
                     }elseif(substr($i, -1) == 3 && substr($i, -2) != 13){
                         
                         $position = $i."RD" ;
