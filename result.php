1b4a6e4bd97a80ff1aac5ce793192afc9b3d3ade
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-08 17:38:00 +0100

    Gitacora saved 1 changed files

--- result.php
+++ result.php
@@ -250,7 +250,7 @@ $num_of_sub = mysqli_num_rows($query);
                         </td>
                         
                         <td>
-                        <??>
+                        <?php ?>
                         </td>
                         
                         <td>
