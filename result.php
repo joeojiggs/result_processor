f3d89e58a092ec3b7113067c3d70f4044c49c464
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-08 15:58:30 +0100

    Gitacora saved 1 changed files

--- result.php
+++ result.php
@@ -199,7 +199,7 @@ $num_of_sub = mysqli_num_rows($query);
                         <?php
                             $query =  mysqli_query($con, "select * from ".$class_name."_".$sub_id." where reg_no = '$reg_no'");
 
-                            $row = mysqli_fetch_array($query)
+                            $row = mysqli_fetch_array($query);
                             echo $row['first test'];
                         ?>
                         </td>
