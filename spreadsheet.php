41ff9077e8d6335c955325db77a668fed38a8261
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-09 14:47:35 +0100

    Gitacora saved 1 changed files

--- spreadsheet.php
+++ spreadsheet.php
@@ -194,6 +194,7 @@ if (isset($_POST['process'])){
 
         if (mysqli_num_rows($query2) > 0){
             while($row2 = mysqli_fetch_array($query2)){
+            
         $reg_no = $row['Reg_Num'];
 
 
