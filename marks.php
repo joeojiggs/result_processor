b9f772c872747cecc94b8938e203141df6a66569
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-09 14:02:05 +0100

    Gitacora saved 1 changed files

--- marks.php
+++ marks.php
@@ -42,7 +42,7 @@ if (isset($_POST['grade'])){
                     $grade = 'F';
                 }
 
-                if ($total != ){
+                if ($total != 0){
                     if ($total != $last_total){
 
                         if(substr($i, -1) == 1 && substr($i, -2) != 11){
