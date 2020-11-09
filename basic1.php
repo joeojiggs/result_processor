0a9d9910dc6a3b405a2fc624dfa50a75027e51f5
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-09 13:37:11 +0100

    Gitacora saved 1 changed files

--- basic1.php
+++ basic1.php
@@ -31,7 +31,7 @@ if(isset($_POST['delete'])){
   $query = mysqli_query($con,"delete from basic1_subject_list where subject_id = '$sub_id'");
 
   $new = 0;
-  $query2 = mysqli_query($con,"update basic1_".$sub_id." set `first test` = '$new', `second test` ='$new', `exam` = '$new', `Total` = '$new', `grade` = '', `` ");
+  $query2 = mysqli_query($con,"update basic1_".$sub_id." set `first test` = '$new', `second test` ='$new', `exam` = '$new', `Total` = '$new', `grade` = '', `position` ");
       
 
   if (empty($query) && empty($query2)){
