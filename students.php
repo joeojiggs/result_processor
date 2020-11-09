af93f3f341a9250ca603567e1b1bd1867c253fa1
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-09 12:36:57 +0100

    Gitacora saved 1 changed files

--- students.php
+++ students.php
@@ -25,7 +25,7 @@ if(isset($_POST['delete'])){
 
     $query9 = mysqli_query($con, "delete from ".$class."_subject8 where reg_no = '$reg_number')");
 
-    $query10 = mysqli_query($con, "delete from ".$class."_subject9 where reg_no = $reg_number')");
+    $query10 = mysqli_query($con, "delete from ".$class."_subject9 where reg_no = '$reg_number')");
 
     $query11 = mysqli_query($con, "delete from ".$class."_subject10 where reg_no = $reg_number')");
 
