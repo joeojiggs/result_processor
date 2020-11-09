3b3ac9e9bd5864d1f575843ffd9412020d95bb6b
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-09 14:06:07 +0100

    Gitacora saved 1 changed files

--- marks.php
+++ marks.php
@@ -111,7 +111,7 @@ if (isset($_POST['grade'])){
         $olodo = 0;
         $query2 = mysqli_query($con,"select * from ".$class_name."_".$sub_id." where Total = '$olodo' "); 
         while($row2 = mysqli_fetch_array($query2)){
-            
+            $total = $row2['Total'];
         };  
         
    
