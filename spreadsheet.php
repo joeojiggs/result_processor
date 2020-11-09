0c5387ed0a4e30094130594bb080c284036e6201
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-09 14:48:01 +0100

    Gitacora saved 1 changed files

--- spreadsheet.php
+++ spreadsheet.php
@@ -195,7 +195,6 @@ if (isset($_POST['process'])){
         if (mysqli_num_rows($query2) > 0){
             while($row2 = mysqli_fetch_array($query2)){
             
-                
             }
         }
         
