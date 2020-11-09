0d4b95180109e5c1c717155b231ae8bc09949fe2
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-09 12:33:27 +0100

    Gitacora saved 1 changed files

--- students.php
+++ students.php
@@ -5,7 +5,7 @@ include('php-includes/check-login-admin.php');
 if(isset($_POST['delete'])){
     $id = mysqli_real_escape_string($con,$_POST['id']);
     $id = mysqli_real_escape_string($con,$_POST['reg_no']);
-    $id = mysqli_real_escape_string($con,$_POST['class']);
+    $class = mysqli_real_escape_string($con,$_POST['class']);
 
     $query=mysqli_query($con,"delete from students where id = '$id'");
 
