549166d0940831aa51b10bddae616d0ed615fd09
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-17 12:46:59 +0100

    Gitacora saved 1 changed files

--- first-term/marks.php
+++ first-term/marks.php
@@ -393,7 +393,7 @@ if (isset($_POST['grade'])){
     <!-- /#wrapper -->
 
     <!-- jQuery -->
-    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="../vendor/jquery/jquery.min.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
     <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
