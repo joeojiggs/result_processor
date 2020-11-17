9cc88df195e6584cb9e51a425d25fc871cd73bc5
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-17 12:58:50 +0100

    Gitacora saved 1 changed files

--- first-term/students.php
+++ first-term/students.php
@@ -62,7 +62,7 @@ if(isset($_POST['delete'])){
     <title>SRPS</title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- MetisMenu CSS -->
     <link href="vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
