86c6d73f4f03a71196a5e1654faff8221d280705
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-17 12:47:32 +0100

    Gitacora saved 1 changed files

--- first-term/prenur.php
+++ first-term/prenur.php
@@ -60,7 +60,7 @@ if(isset($_POST['delete'])){
     <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- MetisMenu CSS -->
-    <link href="vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+    <link href="../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
     <link href="dist/css/sb-admin-2.css" rel="stylesheet">
