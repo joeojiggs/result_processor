8ae62e9e87e3bf70790e6fe1554cc2cb63cdb62c
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-17 12:03:51 +0100

    Gitacora saved 2 changed files

--- dashboard.php
+++ dashboard.php
@@ -104,7 +104,7 @@
                                 </div>
                                 <div class="col-xs-9 text-right">
                                     <div class="huge">3RD TERM</div>
-                                    <div>MAY-AUGUS</div>
+                                    <div>MAY-AUGUST</div>
                                 </div>
                             </div>
                         </div>
