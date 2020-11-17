ae3b213ae0fab7fd813182363b6dc5ffcf80de3a
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-17 12:25:14 +0100

    Gitacora saved 1 changed files

--- dashboard.php
+++ dashboard.php
@@ -108,7 +108,7 @@
                                 </div>
                             </div>
                         </div>
-                        <a href="#">
+                        
                             <div class="panel-footer">
                                 <span class="pull-left">View Details</span>
                                 <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
