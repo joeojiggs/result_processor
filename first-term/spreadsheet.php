3e9c46bb1c4d67665ddc0e28ca515a6be7cbe4dd
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-17 12:49:14 +0100

    Gitacora saved 1 changed files

--- first-term/spreadsheet.php
+++ first-term/spreadsheet.php
@@ -263,7 +263,7 @@ if (isset($_POST['process'])){
     <link href="../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="dist/css/sb-admin-2.css" rel="stylesheet">
+    <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
 
     <!-- Morris Charts CSS -->
     <link href="vendor/morrisjs/morris.css" rel="stylesheet">
