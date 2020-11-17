24dd12d6e4bb7c6d09cd2779b1dfec2fdc26572a
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-17 11:15:57 +0100

    Gitacora saved 15 changed files

--- spreadsheet.php
+++ spreadsheet.php
@@ -526,10 +526,7 @@ if (isset($_POST['process'])){
                         
                     }
                 ?>    
-               </td>
-         
                
-               <td>
                 <?php
                     $osql ="select * from ".$class_name."_subject2 where reg_no = '$Reg_Num'" ;
                     $oquery= mysqli_query($con,$osql);
