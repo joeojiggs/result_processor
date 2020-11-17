bc79d2c944283ec6fcb5f4afea256204b692a6d9
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-17 13:05:24 +0100

    Gitacora saved 1 changed files

--- first-term/spreadsheet.php
+++ first-term/spreadsheet.php
@@ -100,7 +100,7 @@ if (isset($_POST['process'])){
     $i=1;
     $last_avg = "";
     $last_i;
-    $sql ="select * from students where Class='$class_name' and first_total != '$olodo' order by length(firsttotal), total DESC";
+    $sql ="select * from students where Class='$class_name' and first_total != '$olodo' order by length(first_total), total DESC";
     $query= mysqli_query($con,$sql);
         if (mysqli_num_rows($query) > 0) {
 
