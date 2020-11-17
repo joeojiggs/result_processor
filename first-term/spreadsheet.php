b965489b2f1b9284f84c126ebb52deea88815e6b
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-17 12:49:00 +0100

    Gitacora saved 1 changed files

--- first-term/spreadsheet.php
+++ first-term/spreadsheet.php
@@ -1,6 +1,6 @@
 <?php
 include('php-includes/connect.php');
-include('php-includes/check-login-admin.php');
+include('../php-includes/check-login-admin.php');
 
 $class_name = ($_POST['class_name']);
 
