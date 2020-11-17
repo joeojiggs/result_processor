2ffa78f559bf0af4e913cb2d178554ff083375ae
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-17 14:13:43 +0100

    Gitacora saved 1 changed files

--- php-includes/connect.php
+++ php-includes/connect.php
@@ -16,7 +16,7 @@
 	$db_host = "localhost";
 	$db_user = "root";
 	$db_pass = "ojiggs";
-	$db_name = "firstterm20202021session";
+	$db_name = "secondterm20202021session";
 	
 	$con2 =  mysqli_connect($db_host,$db_user,$db_pass,$db_name);
 	if(mysqli_connect_error()){
