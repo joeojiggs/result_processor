b68ff7d8b13a3875c893abbddfded2b309c1feb4
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-17 13:11:08 +0100

    Gitacora saved 1 changed files

--- first-term/spreadsheet.php
+++ first-term/spreadsheet.php
@@ -195,7 +195,7 @@ if (isset($_POST['process'])){
 
         if (mysqli_num_rows($query2) > 0){
             while($row2 = mysqli_fetch_array($query2)){
-                $avg = $row2['firstavg'];
+                $avg = $row2['first_avg'];
                 $reg_no = $row2['Reg_Num'];
 
                 $grade = 'F';
