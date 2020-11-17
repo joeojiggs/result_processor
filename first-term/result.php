3ace8f758ee69948487207d0fa86f7dcd9f70c3e
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-17 12:48:48 +0100

    Gitacora saved 1 changed files

--- first-term/result.php
+++ first-term/result.php
@@ -357,7 +357,7 @@ $num_of_sub = mysqli_num_rows($query);
     <!-- Morris Charts JavaScript -->
     <script src="../vendor/raphael/raphael.min.js"></script>
     <script src="../vendor/morrisjs/morris.min.js"></script>
-    <script src="vendor/morrisjs/myjs.js"></script>
+    <script src="../vendor/morrisjs/myjs.js"></script>
     <script src="data/morris-data.js"></script>
 
     <!-- Custom Theme JavaScript -->
