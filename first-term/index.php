9e5ac0d34f2ebcc38cdf934c63b387278fe3424d
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-17 13:19:21 +0100

    Gitacora saved 1 changed files

--- first-term/index.php
+++ first-term/index.php
@@ -133,6 +133,7 @@ include('../php-includes/check-login-admin.php');
         <div id="page-wrapper" >
             <div class="row">
                 <div class="col-lg-12">
+                
                     <h1 class="page-header">Dashboard</h1>
                 </div>
                 <!-- /.col-lg-12 -->
