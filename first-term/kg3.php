51b490472ad919a5f1b6907492c9368118a1068b
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-17 12:46:19 +0100

    Gitacora saved 1 changed files

--- first-term/kg3.php
+++ first-term/kg3.php
@@ -357,7 +357,7 @@ if(isset($_POST['delete'])){
     <script src="../vendor/raphael/raphael.min.js"></script>
     <script src="../vendor/morrisjs/morris.min.js"></script>
     <script src="../vendor/morrisjs/myjs.js"></script>
-    <script src="data/morris-data.js"></script>
+    <script src="../data/morris-data.js"></script>
 
     <!-- Custom Theme JavaScript -->
     <script src="dist/js/sb-admin-2.js"></script>
