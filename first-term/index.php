600ca9521971714fe3499c2f91844e7d086449c7
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-17 12:08:33 +0100

    Gitacora saved 1 changed files

--- first-term/index.php
+++ first-term/index.php
@@ -99,7 +99,7 @@ include('php-includes/check-login-admin.php');
     <link href="../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="/dist/css/sb-admin-2.css" rel="stylesheet">
+    <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
 
     <!-- Morris Charts CSS -->
     <link href="vendor/morrisjs/morris.css" rel="stylesheet">
