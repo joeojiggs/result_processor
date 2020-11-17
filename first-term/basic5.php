e3903ca7728b495aa0b7e11ab9de878d5053f579
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-17 12:37:00 +0100

    Gitacora saved 1 changed files

--- first-term/basic5.php
+++ first-term/basic5.php
@@ -343,7 +343,7 @@ if(isset($_POST['delete'])){
     <!-- /#wrapper -->
 
     <!-- jQuery -->
-    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="../vendor/jquery/jquery.min.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
     <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
