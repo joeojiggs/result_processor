de1db2abc9c5037307d9465fec1039374e26d8c4
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-17 12:46:45 +0100

    Gitacora saved 1 changed files

--- first-term/marks.php
+++ first-term/marks.php
@@ -181,7 +181,7 @@ if (isset($_POST['grade'])){
     <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- MetisMenu CSS -->
-    <link href="vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+    <link href="../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
     <link href="dist/css/sb-admin-2.css" rel="stylesheet">
