2b2479c97bf48fc6b2e76d87142ea975536defab
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-17 12:24:20 +0100

    Gitacora saved 1 changed files

--- dashboard.php
+++ dashboard.php
@@ -98,23 +98,23 @@
                 <div class="col-lg-3 col-md-6">
                     <div class="panel panel-yellow">
                         <a href="#">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-edit fa-5x"></i>
-                                </div>
-                                <div class="col-xs-9 text-right">
-                                    <div class="huge">3RD TERM</div>
-                                    <div>MAY-AUG</div>
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-edit fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                        <div class="huge">3RD TERM</div>
+                                        <div>MAY-AUG</div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        
-                        <div class="panel-footer">
-                            <span class="pull-left">View Details</span>
-                            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                            <div class="clearfix"></div>
-                        </div>
+                            
+                            <div class="panel-footer">
+                                <span class="pull-left">View Details</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
                         </a>
                     </div>
                 </div>
