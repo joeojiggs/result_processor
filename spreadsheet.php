7fd4df2234e74dec71bb025026ffefc65abbe6bb
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-17 11:13:09 +0100

    Gitacora saved 15 changed files

--- spreadsheet.php
+++ spreadsheet.php
@@ -419,7 +419,9 @@ if (isset($_POST['process'])){
                 $thisquery10 = mysqli_query($con, $thissql10); 
                 $thisrow10 = mysqli_fetch_array($thisquery10);
                 if (isset($thisrow10['subject_name'])){
-                    echo $thisrow10['subject_name'];
+                    ?><th><?php
+                    echo $thisrow1['subject_name'];
+                    ?></th><?php
                 }
             ?>
           
