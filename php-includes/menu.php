b8efacd9211950eed1f5b58418c884e742ca476b
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-17 12:05:08 +0100

    Gitacora saved 1 changed files

--- php-includes/menu.php
+++ php-includes/menu.php
@@ -54,12 +54,12 @@
                             <a href="prenur.php"><i class="fa  fa-mortar-board"></i> First Term </a>
                         </li>
                         <li>
-                            <a href="kg1.php"><i class="fa  fa-user "></i> </a>
+                            <a href="kg1.php"><i class="fa  fa-user "></i> Second Term</a>
                         </li>
                         
                        
                         <li>
-                            <a href="basic1.php"><i class="fa  fa-edit"></i> Basic.1</a>
+                            <a href="basic1.php"><i class="fa  fa-edit"></i> Third Term</a>
                         </li>
                        
                         
