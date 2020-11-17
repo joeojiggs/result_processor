585cd5058e48745b4db9647f73d5dd30931e41ec
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-17 12:49:38 +0100

    Gitacora saved 1 changed files

--- first-term/spreadsheet.php
+++ first-term/spreadsheet.php
@@ -852,7 +852,7 @@ if (isset($_POST['process'])){
     <!-- Morris Charts JavaScript -->
     <script src="../vendor/raphael/raphael.min.js"></script>
     <script src="../vendor/morrisjs/morris.min.js"></script>
-    <script src="vendor/morrisjs/myjs.js"></script>
+    <script src="../vendor/morrisjs/myjs.js"></script>
     <script src="data/morris-data.js"></script>
 
     <!-- Custom Theme JavaScript -->
