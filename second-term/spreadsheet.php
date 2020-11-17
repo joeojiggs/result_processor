ce4f331a2cbebd752cc08e0227e7a49ccd56a67b
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-17 18:23:42 +0100

    Gitacora saved 1 changed files

--- second-term/spreadsheet.php
+++ second-term/spreadsheet.php
@@ -317,7 +317,7 @@ if (isset($_POST['process'])){
             
             <?php 
                 $thissql1 = "select * from ".$class_name."_subject_list where subject_id = 'subject1' ";
-                $thisquery1 = mysqli_query($con, $thissql1); 
+                $thisquery1 = mysqli_query($con2, $thissql1); 
                 $thisrow1 = mysqli_fetch_array($thisquery1);
                 if (isset($thisrow1['subject_name'])){
                     ?><th><?php
