e077474c391f0a095bb2aa18e7c2b07ea55553e8
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-17 11:54:09 +0100

    Gitacora saved 1 changed files

--- dashboard.php
+++ dashboard.php
@@ -45,7 +45,7 @@
         <div id="page-wrapper">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1 class="page-header" aligns= "center">WELCOME</h1>
+                    <h1 class="page-header" align= "center">WELCOME</h1>
                 </div>
                 <!-- /.col-lg-12 -->
             </div>
