b105c783e0ae5471db8a5afba627b448d89f83ab
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-17 11:23:55 +0100

    Gitacora saved 15 changed files

--- spreadsheet.php
+++ spreadsheet.php
@@ -673,6 +673,7 @@ if (isset($_POST['process'])){
                     if (mysqli_num_rows($oquery) > 0) {
 
                         $orow = mysqli_fetch_array($oquery);
+                        if ($orow['Total'] != 0){
                             ?><td><?php
                             echo $orow['Total'] ;
                             ?></td><?php
