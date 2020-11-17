f7d298a7a1ce40f194339a86637fe3f5049e7f08
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-17 12:03:23 +0100

    Gitacora saved 2 changed files

--- dashboard.php
+++ dashboard.php
@@ -82,7 +82,7 @@
                                 </div>
                                 <div class="col-xs-9 text-right">
                                     <div class="huge">2ND TERM</div>
-                                    <div>JAN-MA</div>
+                                    <div>JAN-M</div>
                                 </div>
                             </div>
                         </div>
