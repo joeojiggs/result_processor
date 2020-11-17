60fcd7ce99010eab6c98c3fcdd94ad38660dc315
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-17 13:06:24 +0100

    Gitacora saved 1 changed files

--- first-term/spreadsheet.php
+++ first-term/spreadsheet.php
@@ -158,7 +158,7 @@ if (isset($_POST['process'])){
 
                         $position = $last_i."ST" ;
 
-                        $sql3 = "update students set `first_grade` = '$grade', `position` = '$position' where Class='$class_name' and Reg_Num = '$reg_no'";
+                        $sql3 = "update students set `first_grade` = '$grade', `first_position` = '$position' where Class='$class_name' and Reg_Num = '$reg_no'";
 
                     }elseif(substr($last_i, -1) == 2 && substr($last_i, -2) != 12){
 
