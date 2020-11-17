40fd55a802a07db57fb3ae2f19872853101e97b3
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-17 11:46:49 +0100

    Gitacora saved 1 changed files

--- dashboard.php
+++ dashboard.php
@@ -152,13 +152,7 @@ include('php-includes/check-login-admin.php');
                                 </div>
                             </div>
                         </div>
-                        <a href="#">
-                            <div class="panel-footer">
-                                <span class="pull-left">View Details</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
+                        
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6">
