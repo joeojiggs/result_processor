8a3e27c5fd18f4569657c6af5cb6d9da851a8f66
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-17 12:34:58 +0100

    Gitacora saved 1 changed files

--- first-term/basic3.php
+++ first-term/basic3.php
@@ -347,7 +347,7 @@ if(isset($_POST['delete'])){
     <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
 
     <!-- Metis Menu Plugin JavaScript -->
-    <script src="vendor/metisMenu/metisMenu.min.js"></script>
+    <script src="../vendor/metisMenu/metisMenu.min.js"></script>
 
     <!-- Morris Charts JavaScript -->
     <script src="vendor/raphael/raphael.min.js"></script>
