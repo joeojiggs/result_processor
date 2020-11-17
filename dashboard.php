36fd2ebae8e621d53eb51296de05ae1baa81b651
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-17 11:55:54 +0100

    Gitacora saved 1 changed files

--- dashboard.php
+++ dashboard.php
@@ -81,7 +81,7 @@
                                     <i class="fa fa-tasks fa-5x"></i>
                                 </div>
                                 <div class="col-xs-9 text-right">
-                                    <div class="huge">12</div>
+                                    <div class="huge">SECOND TERM</div>
                                     <div>New Tasks!</div>
                                 </div>
                             </div>
