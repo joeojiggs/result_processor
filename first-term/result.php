110077652418f2fbcc050203394b910d22627e36
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-17 12:48:06 +0100

    Gitacora saved 1 changed files

--- first-term/result.php
+++ first-term/result.php
@@ -1,6 +1,6 @@
 <?php
 include('php-includes/connect.php');
-include('php-includes/check-login-admin.php');
+include('../php-includes/check-login-admin.php');
 
 $class_name = ($_POST['class']);
 $reg_no = ($_POST['reg_no']);
