95cfe360ea472ef2f47bfea68c54756f48422793
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-18 15:18:59 +0100

    Gitacora saved 1 changed files

--- first-term/basic2.php
+++ first-term/basic2.php
@@ -10,7 +10,8 @@ if(isset($_POST['submit'])){
     }elseif(empty($sub_id)){
       echo '<script>alert("Subject is full for this class") </script>';
     }else{
-          $query = mysqli_query($con, "insert into basic2_subject_list(subject_id, subject_name) value ('$sub_id', '$sub_name')");
+      if(empty($query)){
+
     }
     if(empty($query)){
         echo '<script>alert("Failed")</script>';
