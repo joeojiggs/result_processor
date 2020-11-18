7817dcf9da11cab7ce19f90b5ac22accd560467c
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-18 16:24:57 +0100

    Gitacora saved 2 changed files

--- third-term/basic3.php
+++ third-term/basic3.php
@@ -25,7 +25,7 @@ if(isset($_POST['delete'])){
   $sub_id = mysqli_real_escape_string($con3,$_POST['sub_id']);
 
   $query = mysqli_query($con,"delete from basic3_subject_list where subject_id = '$sub_id'");
-  $query = mysqli_query($con2,"delete from basic3_subject_list where subject_id = '$sub_id'");
+  $queryb = mysqli_query($con2,"delete from basic3_subject_list where subject_id = '$sub_id'");
   $query = mysqli_query($con3,"delete from basic3_subject_list where subject_id = '$sub_id'");
 
   $new = 0;
