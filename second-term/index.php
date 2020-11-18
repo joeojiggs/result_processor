3cbb650f49fbe7790b38af8ac4740c569f189043
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-18 13:45:29 +0100

    Gitacora saved 1 changed files

--- second-term/index.php
+++ second-term/index.php
@@ -34,7 +34,7 @@ include('../php-includes/check-login-admin.php');
 
         $query4 = mysqli_query($con, "insert into ".$class."_subject3 (reg_no) values ('$reg_number')");
         $query4b = mysqli_query($con2, "insert into ".$class."_subject3 (reg_no) values ('$reg_number')");
-        //$query4c = mysqli_query($con3, "insert into ".$class."_subject3 (reg_no) values ('$reg_number')");
+        $query4c = mysqli_query($con3, "insert into ".$class."_subject3 (reg_no) values ('$reg_number')");
 
         $query5 = mysqli_query($con, "insert into ".$class."_subject4 (reg_no) values ('$reg_number')");
         $query5b = mysqli_query($con2, "insert into ".$class."_subject4 (reg_no) values ('$reg_number')");
