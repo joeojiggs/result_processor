1ee9ae63f39f8d3bd7268e24bcc2d9ed64401428
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-18 16:11:07 +0100

    Gitacora saved 1 changed files

--- second-term/kg2.php
+++ second-term/kg2.php
@@ -32,7 +32,7 @@ if(isset($_POST['delete'])){
     $new = 0;
     $query2 = mysqli_query($con2,"update kg2_".$sub_id." set `first test` = '$new', `second test` ='$new', `exam` = '$new', `Total` = '$new', `grade` = '', `position` = '' ");
     $query2b = mysqli_query($con2,"update kg2_".$sub_id." set `first test` = '$new', `second test` ='$new', `exam` = '$new', `Total` = '$new', `grade` = '', `position` = '' ");
-    $query2 = mysqli_query($con2,"update kg2_".$sub_id." set `first test` = '$new', `second test` ='$new', `exam` = '$new', `Total` = '$new', `grade` = '', `position` = '' ");
+    $query2c = mysqli_query($con2,"update kg2_".$sub_id." set `first test` = '$new', `second test` ='$new', `exam` = '$new', `Total` = '$new', `grade` = '', `position` = '' ");
         
   
     if (empty($query) && empty($query2)){
