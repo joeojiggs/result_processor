2d27c11e0caa6e030898d8f2cd60bd6250d65518
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-18 16:42:29 +0100

    Gitacora saved 2 changed files

--- third-term/prenur.php
+++ third-term/prenur.php
@@ -36,7 +36,9 @@ if(isset($_POST['delete'])){
 
   $new = 0;
   $query2 = mysqli_query($con3,"update prenur_".$sub_id." set `first test` = '$new', `second test` ='$new', `exam` = '$new', `Total` = '$new', `grade` = '', `position` = '' ");
-        
+  $query2 = mysqli_query($con3,"update prenur_".$sub_id." set `first test` = '$new', `second test` ='$new', `exam` = '$new', `Total` = '$new', `grade` = '', `position` = '' ");
+  $query2 = mysqli_query($con3,"update prenur_".$sub_id." set `first test` = '$new', `second test` ='$new', `exam` = '$new', `Total` = '$new', `grade` = '', `position` = '' ");
+      
 
   if (empty($query) && empty($query2)){
       echo '<script>alert("Failed") </script>';
