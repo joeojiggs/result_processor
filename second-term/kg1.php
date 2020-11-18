905684076c4a9b54f2bb27466f2a5b23b9fe1f79
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-18 16:09:10 +0100

    Gitacora saved 1 changed files

--- second-term/kg1.php
+++ second-term/kg1.php
@@ -26,7 +26,7 @@ if(isset($_POST['delete'])){
     $sub_id = mysqli_real_escape_string($con2,$_POST['sub_id']);
   
     $query = mysqli_query($con2,"delete from kg1_subject_list where subject_id = '$sub_id'");
-    $query = mysqli_query($con2,"delete from kg1_subject_list where subject_id = '$sub_id'");
+    $queryb = mysqli_query($con2,"delete from kg1_subject_list where subject_id = '$sub_id'");
     $query = mysqli_query($con2,"delete from kg1_subject_list where subject_id = '$sub_id'");
   
     $new = 0;
