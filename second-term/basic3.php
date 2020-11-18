047e33010e8ab8a2ed7e566de68c9a780549edb7
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-18 15:56:56 +0100

    Gitacora saved 1 changed files

--- second-term/basic3.php
+++ second-term/basic3.php
@@ -25,6 +25,8 @@ if(isset($_POST['delete'])){
   $sub_id = mysqli_real_escape_string($con2,$_POST['sub_id']);
 
   $query = mysqli_query($con2,"delete from basic3_subject_list where subject_id = '$sub_id'");
+  $query = mysqli_query($con2,"delete from basic3_subject_list where subject_id = '$sub_id'");
+  $query = mysqli_query($con2,"delete from basic3_subject_list where subject_id = '$sub_id'");
 
   $new = 0;
   $query2 = mysqli_query($con2,"update basic3_".$sub_id." set `first test` = '$new', `second test` ='$new', `exam` = '$new', `Total` = '$new', `grade` = '', `position` = '' ");
