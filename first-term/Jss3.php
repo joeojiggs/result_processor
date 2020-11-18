9068078b5caff66e3ce307b536d45d30383ae194
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-18 15:37:13 +0100

    Gitacora saved 1 changed files

--- first-term/Jss3.php
+++ first-term/Jss3.php
@@ -26,7 +26,7 @@ if(isset($_POST['delete'])){
     $sub_id = mysqli_real_escape_string($con,$_POST['sub_id']);
   
     $query = mysqli_query($con,"delete from jss3_subject_list where subject_id = '$sub_id'");
-    $query = mysqli_query($con,"delete from jss3_subject_list where subject_id = '$sub_id'");
+    $queryb = mysqli_query($con,"delete from jss3_subject_list where subject_id = '$sub_id'");
     $query = mysqli_query($con,"delete from jss3_subject_list where subject_id = '$sub_id'");
   
     $new = 0;
