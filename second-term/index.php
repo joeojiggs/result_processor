188ed7f64e8ff4813a8d5d60f6ab1dbb12826dcc
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-18 11:59:57 +0100

    Gitacora saved 1 changed files

--- second-term/index.php
+++ second-term/index.php
@@ -81,7 +81,7 @@ include('../php-includes/check-login-admin.php');
         $query15 = mysqli_query($con, "insert into ".$class."_subject14 (reg_no) values ('$reg_number')");
 
         $query16 = mysqli_query($con, "insert into ".$class."_subject15 (reg_no) values ('$reg_number')");
-        query16 = mysqli_query($con, "insert into ".$class."_subject15 (reg_no) values ('$reg_number')");
+        
         
         if (empty($query)){
             echo "<script> alert('Failed to add student')</script>";
