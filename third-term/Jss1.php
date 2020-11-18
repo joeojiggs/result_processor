b5d9423d4c7a602f43c90f1052a7cd347d5eec96
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-18 16:28:53 +0100

    Gitacora saved 2 changed files

--- third-term/Jss1.php
+++ third-term/Jss1.php
@@ -26,6 +26,8 @@ if(isset($_POST['delete'])){
   $sub_id = mysqli_real_escape_string($con3,$_POST['sub_id']);
 
   $query = mysqli_query($con3,"delete from jss1_subject_list where subject_id = '$sub_id'");
+  $query = mysqli_query($con3,"delete from jss1_subject_list where subject_id = '$sub_id'");
+  $query = mysqli_query($con3,"delete from jss1_subject_list where subject_id = '$sub_id'");
 
   $new = 0;
   $query2 = mysqli_query($con3,"update jss1_".$sub_id." set `first test` = '$new', `second test` ='$new', `exam` = '$new', `Total` = '$new', `grade` = '', `position` = '' ");
