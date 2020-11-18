1fa55eff1c35c4ca03609108c1d9b11033bbf686
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-18 16:01:12 +0100

    Gitacora saved 1 changed files

--- second-term/Jss1.php
+++ second-term/Jss1.php
@@ -31,6 +31,8 @@ if(isset($_POST['delete'])){
 
   $new = 0;
   $query2 = mysqli_query($con2,"update jss1_".$sub_id." set `first test` = '$new', `second test` ='$new', `exam` = '$new', `Total` = '$new', `grade` = '', `position` = '' ");
+  $query2 = mysqli_query($con2,"update jss1_".$sub_id." set `first test` = '$new', `second test` ='$new', `exam` = '$new', `Total` = '$new', `grade` = '', `position` = '' ");
+  $query2 = mysqli_query($con2,"update jss1_".$sub_id." set `first test` = '$new', `second test` ='$new', `exam` = '$new', `Total` = '$new', `grade` = '', `position` = '' ");
       
 
   if (empty($query) && empty($query2)){
