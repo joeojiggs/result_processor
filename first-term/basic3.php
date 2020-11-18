be071f8e9163642ef5abffa612014d385d6ebdcf
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-18 15:22:58 +0100

    Gitacora saved 1 changed files

--- first-term/basic3.php
+++ first-term/basic3.php
@@ -29,7 +29,7 @@ if(isset($_POST['delete'])){
   $new = 0;
   $query2 = mysqli_query($con,"update basic3_".$sub_id." set `first test` = '$new', `second test` ='$new', `exam` = '$new', `Total` = '$new', `grade` = '', `position` = '' ");
   $query2 = mysqli_query($con,"update basic3_".$sub_id." set `first test` = '$new', `second test` ='$new', `exam` = '$new', `Total` = '$new', `grade` = '', `position` = '' ");
-        
+  $query2 = mysqli_query($con,"update basic3_".$sub_id." set `first test` = '$new', `second test` ='$new', `exam` = '$new', `Total` = '$new', `grade` = '', `position` = '' ");      
 
   if (empty($query) && empty($query2)){
       echo '<script>alert("Failed") </script>';
