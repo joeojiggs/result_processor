9a6ab97f70391348495dfa91d9d4dd482b2cb952
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-18 15:39:02 +0100

    Gitacora saved 1 changed files

--- first-term/kg1.php
+++ first-term/kg1.php
@@ -31,8 +31,8 @@ if(isset($_POST['delete'])){
   
     $new = 0;
     $query2 = mysqli_query($con,"update kg1_".$sub_id." set `first test` = '$new', `second test` ='$new', `exam` = '$new', `Total` = '$new', `grade` = '', `position` = '' ");
-    $query2b = mysqli_query($con,"update kg1_".$sub_id." set `first test` = '$new', `second test` ='$new', `exam` = '$new', `Total` = '$new', `grade` = '', `position` = '' ");
-    $query2c = mysqli_query($con,"update kg1_".$sub_id." set `first test` = '$new', `second test` ='$new', `exam` = '$new', `Total` = '$new', `grade` = '', `position` = '' ");
+    $query2b = mysqli_query($con2,"update kg1_".$sub_id." set `first test` = '$new', `second test` ='$new', `exam` = '$new', `Total` = '$new', `grade` = '', `position` = '' ");
+    $query2c = mysqli_query($con3,"update kg1_".$sub_id." set `first test` = '$new', `second test` ='$new', `exam` = '$new', `Total` = '$new', `grade` = '', `position` = '' ");
         
   
     if (empty($query) && empty($query2)){
