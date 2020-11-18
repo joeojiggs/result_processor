ce2c6b7eae5489d21cefa1c02930d0a2c8d78709
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-18 15:47:44 +0100

    Gitacora saved 1 changed files

--- first-term/SSS2.php
+++ first-term/SSS2.php
@@ -27,7 +27,7 @@ if(isset($_POST['delete'])){
 
   $query = mysqli_query($con,"delete from sss2_subject_list where subject_id = '$sub_id'");
   $queryb = mysqli_query($con,"delete from sss2_subject_list where subject_id = '$sub_id'");
-  $query = mysqli_query($con,"delete from sss2_subject_list where subject_id = '$sub_id'");
+  $queryc = mysqli_query($con,"delete from sss2_subject_list where subject_id = '$sub_id'");
 
   $new = 0;
   $query2 = mysqli_query($con,"update sss2_".$sub_id." set `first test` = '$new', `second test` ='$new', `exam` = '$new', `Total` = '$new', `grade` = '', `position` = '' ");
