6387ded9b012f9d1abd3f41cf9ec089065c30c3d
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-18 16:43:13 +0100

    Gitacora saved 2 changed files

--- third-term/SSS1.php
+++ third-term/SSS1.php
@@ -10,9 +10,9 @@ if(isset($_POST['submit'])){
     }elseif(empty($sub_id)){
       echo '<script>alert("Subject is full for this class") </script>';
     }else{
-      $query = mysqli_query($con3, "insert into SSS1_subject_list(subject_id, subject_name) value ('$sub_id', '$sub_name')");
+      $query = mysqli_query($con, "insert into SSS1_subject_list(subject_id, subject_name) value ('$sub_id', '$sub_name')");
       $queryb = mysqli_query($con3, "insert into SSS1_subject_list(subject_id, subject_name) value ('$sub_id', '$sub_name')");
-      $query = mysqli_query($con3, "insert into SSS1_subject_list(subject_id, subject_name) value ('$sub_id', '$sub_name')");
+      $queryc = mysqli_query($con3, "insert into SSS1_subject_list(subject_id, subject_name) value ('$sub_id', '$sub_name')");
     }
 
     if(empty($query)){
