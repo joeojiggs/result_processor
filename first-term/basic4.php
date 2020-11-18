4f38c07a1a5ec4e1aeb9e87337ae6cebd33f5dec
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-18 15:23:59 +0100

    Gitacora saved 1 changed files

--- first-term/basic4.php
+++ first-term/basic4.php
@@ -12,7 +12,7 @@ if(isset($_POST['submit'])){
     }else{
           $query = mysqli_query($con, "insert into basic4_subject_list(subject_id, subject_name) value ('$sub_id', '$sub_name')");
           $queryb = mysqli_query($con2, "insert into basic4_subject_list(subject_id, subject_name) value ('$sub_id', '$sub_name')");
-          $queryc = mysqli_query($con, "insert into basic4_subject_list(subject_id, subject_name) value ('$sub_id', '$sub_name')");
+          $queryc = mysqli_query($con3, "insert into basic4_subject_list(subject_id, subject_name) value ('$sub_id', '$sub_name')");
     }
 
     if(empty($query)){
