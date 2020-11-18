a96f162cd846aa82114041e67348433ce3109a98
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-18 16:01:58 +0100

    Gitacora saved 1 changed files

--- second-term/jss2.php
+++ second-term/jss2.php
@@ -10,7 +10,7 @@ if(isset($_POST['submit'])){
     }elseif(empty($sub_id)){
       echo '<script>alert("Subject is full for this class") </script>';
     }else{
-          $query = mysqli_query($con2, "insert into Jss2_subject_list(subject_id, subject_name) value ('$sub_id', '$sub_name')");
+          $query = mysqli_query($con, "insert into Jss2_subject_list(subject_id, subject_name) value ('$sub_id', '$sub_name')");
           $queryb = mysqli_query($con2, "insert into Jss2_subject_list(subject_id, subject_name) value ('$sub_id', '$sub_name')");
           $queryc = mysqli_query($con2, "insert into Jss2_subject_list(subject_id, subject_name) value ('$sub_id', '$sub_name')");
         }
