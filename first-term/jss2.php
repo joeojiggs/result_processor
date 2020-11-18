9d28ee545a37ab8a0b91aa71d1fbfbd4c3e3fb8e
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-18 15:27:21 +0100

    Gitacora saved 1 changed files

--- first-term/jss2.php
+++ first-term/jss2.php
@@ -12,7 +12,7 @@ if(isset($_POST['submit'])){
     }else{
           $query = mysqli_query($con, "insert into Jss2_subject_list(subject_id, subject_name) value ('$sub_id', '$sub_name')");
           $query = mysqli_query($con, "insert into Jss2_subject_list(subject_id, subject_name) value ('$sub_id', '$sub_name')");
-          
+          $query = mysqli_query($con, "insert into Jss2_subject_list(subject_id, subject_name) value ('$sub_id', '$sub_name')");
     }
 
     if(empty($query)){
