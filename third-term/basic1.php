6e59b00c00aa00083d80132070ca4024f01178da
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-18 16:48:25 +0100

    Gitacora saved 1 changed files

--- third-term/basic1.php
+++ third-term/basic1.php
@@ -31,10 +31,12 @@ if(isset($_POST['delete'])){
   $sub_id = mysqli_real_escape_string($con3,$_POST['sub_id']);
 
   $query = mysqli_query($con,"delete from basic1_subject_list where subject_id = '$sub_id'");
-  $queryb = mysqli_query($con3,"delete from basic1_subject_list where subject_id = '$sub_id'");
+  $queryb = mysqli_query($con2,"delete from basic1_subject_list where subject_id = '$sub_id'");
   $queryc = mysqli_query($con3,"delete from basic1_subject_list where subject_id = '$sub_id'");
 
   $new = 0;
+  $query2 = mysqli_query($con,"update basic1_".$sub_id." set `first test` = '$new', `second test` ='$new', `exam` = '$new', `Total` = '$new', `grade` = '', `position` = '' ");
+  $query2 = mysqli_query($con2,"update basic1_".$sub_id." set `first test` = '$new', `second test` ='$new', `exam` = '$new', `Total` = '$new', `grade` = '', `position` = '' ");
   $query2 = mysqli_query($con3,"update basic1_".$sub_id." set `first test` = '$new', `second test` ='$new', `exam` = '$new', `Total` = '$new', `grade` = '', `position` = '' ");
       
 
