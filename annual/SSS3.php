acb9482ac25685512e28a412b3c7c1935bc8f18e
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-21 19:29:58 +0100

    Gitacora saved 1 changed files

--- annual/SSS3.php
+++ annual/SSS3.php
@@ -147,7 +147,7 @@ if(isset($_POST['delete'])){
                     <input name="class_name" value="SSS3" type="hidden" >
 
                     <!-- Change this to a button or input when using this as a form -->
-                    <input type="submit" name="seemarks" value="See Marks"class="btn btn-primary btn-block">
+                    <input type="submit" name="seemarks" value="Annual Computation"class="btn btn-primary btn-block">
                 
                </form>
                </td>
