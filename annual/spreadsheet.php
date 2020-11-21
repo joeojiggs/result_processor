df10dcb939b0c7e498fcbdeb5a1dbd1f83b774de
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-22 00:05:12 +0100

    Gitacora saved 1 changed files

--- annual/spreadsheet.php
+++ annual/spreadsheet.php
@@ -629,7 +629,7 @@ if (isset($_POST['process'])){
                         $orow = mysqli_fetch_array($oquery);
                         if (isset($thisrow8['subject_name'])){
                             ?><td><?php
-                            //echo $orow['grand_total'] ;
+                            echo $orow['grand_total'] ;
                             ?></td><?php
                         }
                         
