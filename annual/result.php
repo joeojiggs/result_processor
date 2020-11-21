c8a827dab08f85530414e87e53a6c9fe44f170ff
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-22 00:16:19 +0100

    Gitacora saved 1 changed files

--- annual/result.php
+++ annual/result.php
@@ -203,7 +203,9 @@ $num_of_sub = mysqli_num_rows($query);
                             $row = mysqli_fetch_array($query);
                             echo $row['first test'];
 
-                            
+                            $query2 = mysqli_query($con,"select * from ".$class_name."_".$sub_id." where reg_no =".$row['reg_no']);
+                 $row2 = mysqli_fetch_array($query2); 
+                 echo $row2['Total'];
                         ?>
                         </td>
                         <td>
