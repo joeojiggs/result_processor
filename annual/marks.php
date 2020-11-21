a2df2bdde47527d9454c5c82a588b2b4f2daaff5
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-21 22:31:38 +0100

    Gitacora saved 1 changed files

--- annual/marks.php
+++ annual/marks.php
@@ -54,7 +54,7 @@ if (isset($_POST['grade'])){
     $i=1;
     $last_total = "";
     $last_i;
-    $sql ="select * from ".$class_name."_".$sub_id." where grand_total != '$olodo' order by length(Total), Total DESC";
+    $sql ="select * from ".$class_name."_".$sub_id." where grand_total != '$olodo' order by length(otal), Total DESC";
     $query= mysqli_query($con,$sql);
         if (mysqli_num_rows($query) > 0) {
 
