89dd1a10521db50198d6e7c5e24d5820ad7d6dc4
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-21 22:33:33 +0100

    Gitacora saved 1 changed files

--- annual/marks.php
+++ annual/marks.php
@@ -50,7 +50,7 @@ if (isset($_POST['grade'])){
 
 
     // run grading and position
-    // varable olodo use to prevent 0 coming out first and variable last_total and last_i use to gran
+    // varable olodo use to prevent 0 coming out first and variable last_total and last_i use to grade 
     $olodo = 0;
     $i=1;
     $last_total = "";
