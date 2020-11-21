ac905c6f8ca6182256fc00ad90bb75e0b1802ed3
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-21 19:57:02 +0100

    Gitacora saved 1 changed files

--- annual/marks.php
+++ annual/marks.php
@@ -295,7 +295,7 @@ if (isset($_POST['grade'])){
                <?php echo $row['position']?>
                </td>
                <td>
-               
+               </>
                </tr>
             
           <?php
