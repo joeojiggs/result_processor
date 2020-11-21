28c571db6aa62039b57922aa77e0701c60cb210e
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-21 22:41:21 +0100

    Gitacora saved 1 changed files

--- annual/marks.php
+++ annual/marks.php
@@ -162,7 +162,7 @@ if (isset($_POST['grade'])){
         
                     $position = $i."ST" ;
     
-                    $sql3 = "update ".$class_name."_".$sub_id." set `grade` = '$grade', `position` = '$position' where reg_no = '$reg_no'";
+                    $sql3 = "update ".$class_name."_".$sub_id." set `grand_grade` = '$grade', `position` = '$position' where reg_no = '$reg_no'";
     
                 }elseif(substr($i, -1) == 2 && substr($i, -2) != 12){
     
