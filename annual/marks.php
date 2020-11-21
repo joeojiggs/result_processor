c066d4b30870b258fdc9dba63eed9db0fda42d02
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-22 00:03:44 +0100

    Gitacora saved 1 changed files

--- annual/marks.php
+++ annual/marks.php
@@ -355,7 +355,7 @@ if (isset($_POST['grade'])){
                </td>
                <td>
                <?php 
-               /*
+               
                $query2 = mysqli_query($con,"select * from ".$class_name."_".$sub_id." where reg_no =".$row['reg_no']);
                $row2 = mysqli_fetch_array($query2); 
                echo $row2['grand_grade'];
