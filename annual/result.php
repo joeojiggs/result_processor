7082d2a782287843edf23e843d1bf3f71cc00b90
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-22 00:18:02 +0100

    Gitacora saved 1 changed files

--- annual/result.php
+++ annual/result.php
@@ -233,7 +233,7 @@ $num_of_sub = mysqli_num_rows($query);
                         </td>
                         
                         <td>
-                        <?php echo $row['grade']; ?>
+                        <?php echo $row['grandgrade']; ?>
                         </td>
                         <td>
                         <?php echo $row['position']; ?>
