10e829271a51c41bcb0a25c28ea706238b560f38
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-21 22:27:31 +0100

    Gitacora saved 1 changed files

--- annual/marks.php
+++ annual/marks.php
@@ -39,7 +39,7 @@ if (isset($_POST['grade'])){
 
                 $avg = $grand_total/3;
 
-                 $sql4 = "update ".$class_name."_".$sub_id." set `grand_total` = '$grand_total', `grand_avg` = '$avg' where Reg_Num = '$reg_no' ";
+                 $sql4 = "update ".$class_name."_".$sub_id." set `grand_total` = '$grand_total', `grand_avg` = '$avg' where Reg_Num = '$reg_no'";
                  $query4 = mysqli_query ($con, $sql4);
                         
                     
