509784543cd93bda00d9baf5a19f39189933f391
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-21 22:51:58 +0100

    Gitacora saved 1 changed files

--- annual/spreadsheet.php
+++ annual/spreadsheet.php
@@ -84,7 +84,7 @@ if (isset($_POST['process'])){
                 $num_of_sub = mysqli_num_rows($wuery2);
 
                 $avg = $grand_total/$num_of_sub;
-                $avg = $avg/3;
+                $grandavg = $avg/3;
 
                  $sql4 = "update students set `first_total` = '$grand_total', `first_avg` = '$avg' where Reg_Num = '$reg_no' and Class='$class_name'";
                  $query4 = mysqli_query ($con, $sql4);
