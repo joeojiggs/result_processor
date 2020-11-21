88e6ba577ed2182c3981c8e7a770da24592f90e2
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-21 22:56:53 +0100

    Gitacora saved 1 changed files

--- annual/spreadsheet.php
+++ annual/spreadsheet.php
@@ -165,7 +165,7 @@ if (isset($_POST['process'])){
 
                         $position = $last_i."ND" ;
 
-                        $sql3 = "update students set `first_grade` = '$grade', `first_position` = '$position' where Class='$class_name' and Reg_Num = '$reg_no'";
+                        $sql3 = "update students set `grand_grade` = '$grade', `first_position` = '$position' where Class='$class_name' and Reg_Num = '$reg_no'";
                     
                     }elseif(substr($last_i, -1) == 3 && substr($last_i, -2) != 13){
                         
