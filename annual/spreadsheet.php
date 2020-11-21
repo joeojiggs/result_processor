0930dd0ed052eea2bb591daac3e934b4ab91bb5e
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-21 22:46:27 +0100

    Gitacora saved 1 changed files

--- annual/spreadsheet.php
+++ annual/spreadsheet.php
@@ -48,7 +48,7 @@ if (isset($_POST['process'])){
 
                 $wuery2 = mysqli_query($con,"select * from ".$class_name."_subject8 where reg_no = '$reg_no' "); 
                 $low2 = mysqli_fetch_array($wuery2);  
-                $total8 = $low2['grand_otal'];
+                $total8 = $low2['grand_total'];
 
                 $wuery2 = mysqli_query($con,"select * from ".$class_name."_subject9 where reg_no = '$reg_no' "); 
                 $low2 = mysqli_fetch_array($wuery2);  
