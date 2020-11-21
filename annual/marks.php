90e9efac4d89f752be448e15b42ba69baf76c189
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-21 22:16:32 +0100

    Gitacora saved 1 changed files

--- annual/marks.php
+++ annual/marks.php
@@ -12,7 +12,7 @@ if (isset($_POST['grade'])){
     $class_name = mysqli_real_escape_string($con,$_POST['class_name']);
     
     
-    //
+    //run 
     // run grading and position
     $olodo = 0;
     $i=1;
