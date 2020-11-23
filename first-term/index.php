42b5765e266beb26159b86cb97e303ca4a4445b3
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-23 17:35:28 +0100

    Gitacora saved 1 changed files

--- first-term/index.php
+++ first-term/index.php
@@ -226,7 +226,7 @@ include('../php-includes/check-login-admin.php');
                                 </div>
                             </div>
 
-                </div>
+                        </div>
             <div class="row" >
 
                             <div class="col-lg-3 col-md-3 col-sm-6 col-xm-12">
